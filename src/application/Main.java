e9eb6cf8b8ebc4231a4479021c678b9e4dc91c4b
Patrick Borges <patrickpborges1996@gmail.com>
2026-07-13 20:34:09 -0300

    findById implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,12 +10,9 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
+       SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
-
-        //Injeção de dependência sem explicitar a implementação;
-        SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,6 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 //Classe com operações estáticas para instanciar os DAOs
@@ -7,6 +8,6 @@ public class DaoFactory {
 
     //A classe DaoFactory expõe um metodo que retorna o tipo da interface, mas internamente ela instancia uma implementação;
     public static SellerDao createSellerDao(){
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }
